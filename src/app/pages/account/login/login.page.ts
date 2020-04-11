a021c856fb2129866871132d62027009a08c5fd2
Pedro Ivo de Sales Santos <pedroivo_sales@hotmail.com>
2020-04-11 20:26:16 -0300

    login

---
+++ src/app/pages/account/login/login.page.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { LoadingController, ToastController, NavController } from '@ionic/angular';
+import { DataService } from 'src/app/data.service';
+import { UserModel } from 'src/app/models/user.model';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+  public hide = true;
+  public form: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private loadingCtrl: LoadingController,
+    private toastCtrl: ToastController,
+    private navCtrl: NavController,
+    private service: DataService
+  ) {
+    this.form = this.fb.group({
+      username: ['', Validators.compose([
+        Validators.required
+      ])],
+      password: ['', Validators.compose([
+        Validators.minLength(6),
+        Validators.maxLength(20),
+        Validators.required
+      ])]
+    });
+  }
+
+  async submit() {
+    if (this.form.invalid)
+      return;
+
+    const loading = await this.loadingCtrl.create({ message: 'Autenticando...' });
+    loading.present();
+
+    this
+      .service
+      .authenticate(this.form.value)
+      .subscribe(
+        (res: UserModel) => {
+          //SecurityUtil.set(res);
+          loading.dismiss();
+          this.navCtrl.navigateRoot('/');
+        },
+        (err) => {
+          this.showError("Usuário ou senha inválidos");
+          loading.dismiss();
+        }
+      )
+  }
+
+  ngOnInit() {
+  }
+
+  toggleHide() {
+    this.hide = !this.hide;
+  }
+
+  async showError(message) {
+    const error = await this.toastCtrl.create({ message: message, duration: 3000 });
+    error.present();
+  }
+}
