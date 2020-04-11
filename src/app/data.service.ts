4d6e64500f53bdffe77180552067380c3b1f6c18
Pedro Ivo de Sales Santos <pedroivo_sales@hotmail.com>
2020-04-11 17:04:46 -0300

    Serviço acesso dados

---
+++ src/app/data.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class DataService {
+    public baseUrl = "http://localhost:7188";
+
+    /**
+     *
+     */
+    constructor(private http: HttpClient) {
+    }
+
+    public authenticate(data: any) {
+        return this.http.post(`${this.baseUrl}/v1/login`, data);
+    }
+
+    public getMonthlySalesChartData() {
+        return this.http.get(`${this.baseUrl}/v1/reports/ms`);
+    }
+
+    public getOrders() {
+        return this.http.get(`${this.baseUrl}/v1/orders`);
+    }
+
+    public getOrder(order: string) {
+        return this.http.get(`${this.baseUrl}/v1/orders/${order}`);
+    }
+}
