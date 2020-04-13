041ae546fed9a8e893eb4abf9507cb6042e51446
Pedro Ivo de Sales Santos <pedroivo_sales@hotmail.com>
2020-04-12 21:35:40 -0300

    order details e status util

--- src/app/pages/store/order-details/order-details.page.ts
+++ src/app/pages/store/order-details/order-details.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/data.service';
+import { ActivatedRoute } from '@angular/router';
+import { SecurityUtil } from 'src/app/utils/security.util';
+import { StatusUtil } from 'src/app/utils/status.util';
 
 @Component({
   selector: 'app-order-details',
@@ -6,10 +10,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order-details.page.scss'],
 })
 export class OrderDetailsPage implements OnInit {
+  public order = null;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private service: DataService
+  ) { }
 
   ngOnInit() {
+    let number = this.route.snapshot.paramMap.get('number');
+    this.service
+      .getOrder(number)
+      .subscribe((data: any) => {
+        this.order = data;
+      })
+  }
+
+  isManager(): boolean {
+    return SecurityUtil.isInRole('manager');
+  }
+
+  translateOrderStatus(status: string): string {
+    return StatusUtil.Convert(status);
   }
 
 }
